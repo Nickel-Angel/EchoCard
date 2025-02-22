d7a0bdbe8ef8069b9b033797dc46970c5a7cb8c7
Nickel-Angel <1239004072@qq.com>
2025-02-22 23:37:30 +0800

    add material ui, update CardMemoMain

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,82 @@ import "./App.css";
 import CardMemoMain from "./CardMemo/CardMemoMain";
 import CardEditMain from "./CardEdit/CardEditMain";
 import SettingsMain from "./Settings/SettingsMain";
-import Sidebar from "./Sidebar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import ImportContactsOutlinedIcon from "@mui/icons-material/ImportContactsOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 
-function App() {
-  const [activeComponent, setActiveComponent] = useState('CardMemo');
-  const [sidebarVisible, setSetbarVisible] = useState(true);
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
 
-  const renderComponent = () => {
-    switch(activeComponent) {
-      case 'CardMemo':
-        return <CardMemoMain />;
-      case 'CardEdit':
-        return <CardEditMain />;
-      case 'Settings':
-        return <SettingsMain />;
-      default:
-        return <CardMemoMain />;
-    }    
-  }
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
   return (
-    <main className="container">
-      {sidebarVisible && (<div className="sidebar-container">
-        <Sidebar
-        setActiveComponent={setActiveComponent} />
-        </div>
+    <div
+      className="tabpanel"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3, height: "93%" }}>
+          <Typography sx={{ height: "100%" }}>{children}</Typography>
+        </Box>
       )}
-      <div className="content-container">{renderComponent()}</div>
-    </main>
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+}
+
+function App() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1, bgcolor: "background.paper", display: "flex" }}>
+      <Tabs
+        orientation="vertical"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: "divider" }}
+      >
+        <Tab
+          icon={<ImportContactsOutlinedIcon />}
+          label="牌组记忆"
+          {...a11yProps(0)}
+        />
+        <Tab icon={<EditOutlinedIcon />} label="牌组编辑" {...a11yProps(1)} />
+        <Tab icon={<SettingsOutlinedIcon />} label="设置" {...a11yProps(2)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <CardMemoMain />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <CardEditMain />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <SettingsMain />
+      </TabPanel>
+    </Box>
   );
 }
 
--- src/CardMemo/CardMemoMain.tsx
+++ src/CardMemo/CardMemoMain.tsx
@@ -1,10 +1,102 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material";
 
+interface DeckData {
+  name: string;
+  tolearn: number;
+  learning: number;
+  toreview: number;
+}
+
+function createDeckData(
+  name: string,
+  tolearn: number,
+  learning: number,
+  toreview: number
+): DeckData {
+  return { name, tolearn, learning, toreview };
+}
+
+const TextButton = styled(Button)({
+  textTransform: "none",
+  justifyContent: "flex-start",
+});
+
+function DenseTable({ rows }: any) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>牌组</TableCell>
+            <TableCell align="center">未学习</TableCell>
+            <TableCell align="center">学习中</TableCell>
+            <TableCell align="center">待复习</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row: DeckData) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                <TextButton>{row.name}</TextButton>
+              </TableCell>
+              <TableCell align="center">{row.tolearn}</TableCell>
+              <TableCell align="center">{row.learning}</TableCell>
+              <TableCell align="center">{row.toreview}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 function CardMemoMain() {
-  
+  // TODO: get deck data from back-end
+  const rows: DeckData[] = [
+    createDeckData("Frozen yoghurt", 159, 6, 24),
+    createDeckData("Ice cream sandwich", 237, 9, 37),
+    createDeckData("Eclair", 262, 16, 24),
+    createDeckData("Cupcake", 305, 3, 67),
+    createDeckData("Gingerbread", 356, 16, 49),
+  ];
+  // TODO: get learning time and learning number from back-end
+  const learningTime = 0;
+  const learningNumber = 0;
+  if (rows.length !== 0) {
+    return (
+      <div>
+        <DenseTable rows={rows} />
+        <p style={{ paddingTop: "20px" }}>
+          今天共学习了 {learningTime} 分钟，学习了 {learningNumber} 张卡片。
+        </p>
+      </div>
+    );
+  }
   return (
-    <div>
-      CardMemo
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <p style={{ paddingTop: "20px", textAlign: "center" }}>
+        你还没有卡组哦，快去添加卡组吧~
+        <br />
+        <br />
+        <Button variant="contained">添加卡组</Button>
+      </p>
     </div>
   );
 }
